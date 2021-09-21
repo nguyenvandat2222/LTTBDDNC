65ddc802370457ffc60eab80c193ddd7d30858f9
NVD <nguyenvandat.2222.vn@gmail.com>
2021-09-21 11:30:06 +0700

    first commit

---
+++ Montion_Layout/app/src/main/java/com/example/montion_layout/KTPM14ATT_Activity.java
@@ -0,0 +1,14 @@
+package com.example.montion_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KTPM14ATT_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ktpm14_att);
+    }
+}
